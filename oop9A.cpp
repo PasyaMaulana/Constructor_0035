8fe9f82beb8fa70a81cbe58ee58089d0b33a1dbb
PasyaMaulana <pasyamaulana17@gmail.com>
2025-05-17 17:42:20 +0700

    membuat class dosen dengan akses modifier public dan private

--- oop9A.cpp
+++ oop9A.cpp
@@ -26,3 +26,33 @@ public:
 
     friend class Dosen;
 };
+
+class Dosen
+{
+private:
+    string pangkat;
+    int gaji;
+
+    friend class Staff;
+    friend float lihatGajiDosen(Dosen *d);
+
+public:
+    string nama;
+    string NIDN;
+
+    Dosen(string nama, string NIDN, string pangkat, int gaji) : nama(nama), NIDN(NIDN), pangkat(pangkat), gaji(gaji) {}
+
+    void beriNilai(Mahasiswa *m, float nilai)
+    {
+        m->nilai = nilai;
+    }
+
+    void tampilkanDosen()
+    {
+        cout << "Dosen: " << nama << endl;
+        cout << "NIDN: " << NIDN << endl;
+        cout << "Pangkat: " << pangkat << endl;
+        cout << "Gaji: Rp." << gaji << endl;
+        cout << endl;
+    }
+};
