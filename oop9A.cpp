a5daeb57840ca7f5142b109374bd35cede994e0b
PasyaMaulana <pasyamaulana17@gmail.com>
2025-05-17 17:41:22 +0700

    mendeklarasikan class dan membuat class Mahasiswa dengan akses modifier public dan private

--- oop9A.cpp
+++ oop9A.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Dosen;
+class Staff;
+class Universitas;
+class Mahasiswa
+{
+private:
+    float nilai;
+
+public:
+    string nama;
+    string NIM;
+
+    Mahasiswa(string nama, string NIM) : nama(nama), NIM(NIM), nilai(0) {}
+
+    void tampilkanMahasiswa()
+    {
+        cout << "Mahasiswa: " << nama << endl;
+        cout << "NIM: " << NIM << endl;
+        cout << "Nilai: " << nilai << endl;
+        cout << endl;
+    }
+
+    friend class Dosen;
+};
